cc8c75493112e04c1141b5626a8aa651ec05abcd
Alexis Leclerc <alexis.leclerc.1337@gmail.com>
2016-10-27 14:02:35 -0400

    New loltest for L6470 lib

--- stepper/stepper_controller/src/loltest.py
+++ stepper/stepper_controller/src/loltest.py
@@ -3,28 +3,22 @@
 # Sleep function
 from time import sleep
 
-# SpiDev
-import spidev
+# Stepper controller
+from L6470_lib import L6470
 
 if __name__ == '__main__':
-    # Open the given SPI device
-    device = spidev.SpiDev()
-    device.open(0, 0)
+    # Open SPI controller.
+    _controller = L6470()
+    _controller.open(0, 0)
+
+    # Issue a run command
+    _controller.run(L6470.DIR_CLOCKWISE, 100.0)
     
-    # Configure SPI
-    device.bits_per_word = 8
-    device.max_speed_hz = 1000000
-    device.mode = 0b11
+    # Wait a bit
+    sleep(2)
     
-    # Read register MAX_SPEED with 3 nops for response.
-    request = [0x27, 0x00, 0x00, 0x00]
-    responseBytes = device.xfer2(request)
+    # Softly stop the stepper
+    _controller.softStop()
     
-    # Return only useful 3 bytes.
-    response = responseBytes[3]
-    response = Response | responseBytes[2] << 8
-    response = Response | responseBytes[1] << 16
-    
-    print "Max speed: %d" % (response)
-    
-    device.close()
+    # Close the SPI connection
+    _controller.close()
